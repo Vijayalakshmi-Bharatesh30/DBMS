a0722a794adc0c616e46a559e5e48e2f2ba6fb13
Vijayalakshmi-Bharatesh30 <vijayalakshmi.cs25@bmsce.ac.in>
2025-11-28 09:26:08 +0530

    Add files via upload

--- Supplier database.sql
+++ Supplier database.sql
@@ -66,3 +66,96 @@ AND C.sid = S.sid
 AND C.cost = (SELECT MAX (C1.cost)
 FROM Catalog C1
 WHERE C1.pid = P.pid);
+
+
+SELECT P.pname, S.sname, C.cost
+FROM PARTS P, SUPPLIERS S, CATALOG C
+WHERE P.pid = C.pid
+AND S.sid = C.sid
+AND C.cost = (SELECT MAX(cost) FROM CATALOG);
+
+SELECT S.sname
+FROM Suppliers S
+WHERE S.sid NOT IN (
+    SELECT DISTINCT C.sid
+    FROM Catalog C, Parts P
+    WHERE C.pid = P.pid
+    AND P.color = 'red');
+        
+SELECT S.sname, SUM(C.cost) AS total_value
+FROM Suppliers S, Catalog C
+WHERE S.sid = C.sid
+GROUP BY S.sname;
+
+SELECT S.sname
+FROM Suppliers S, Catalog C
+WHERE S.sid = C.sid
+AND C.cost < 20
+GROUP BY S.sname
+HAVING COUNT(DISTINCT C.pid) >= 2;
+
+
+
+SELECT P.pname, S.sname, C.cost
+FROM Parts P, Suppliers S, Catalog C
+WHERE P.pid = C.pid
+AND S.sid = C.sid
+AND C.cost = (
+    SELECT MIN(C1.cost)
+    FROM Catalog C1
+    WHERE C1.pid = P.pid);
+    
+    
+CREATE VIEW Supplier_Part_Count AS
+SELECT S.sid, S.sname, COUNT(DISTINCT C.pid) AS total_parts
+FROM Suppliers S
+LEFT JOIN Catalog C ON S.sid = C.sid
+GROUP BY S.sid, S.sname;
+
+SELECT * FROM Supplier_Part_Count;
+
+CREATE VIEW Most_Expensive_Supplier_Per_Part AS
+SELECT P.pid, P.pname, S.sid, S.sname, C.cost
+FROM Parts P
+JOIN Catalog C ON P.pid = C.pid
+JOIN Suppliers S ON S.sid = C.sid
+WHERE C.cost = (
+    SELECT MAX(C1.cost)
+    FROM Catalog C1
+    WHERE C1.pid = P.pid);
+SELECT * FROM Most_Expensive_Supplier_Per_Part;
+
+
+DELIMITER $$
+
+CREATE TRIGGER trg_check_catalog_cost
+BEFORE INSERT ON Catalog
+FOR EACH ROW
+BEGIN
+    IF NEW.cost < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cost cannot be less than 1';
+    END IF;
+END$$
+
+DELIMITER ;
+
+INSERT INTO Catalog VALUES(10001,20001,5);
+
+
+DELIMITER $$
+
+CREATE TRIGGER trg_set_default_cost
+BEFORE INSERT ON Catalog
+FOR EACH ROW
+BEGIN
+    IF NEW.cost IS NULL THEN
+        SET NEW.cost = 10;
+    END IF;
+END$$
+                                                                                                             
+DELIMITER ;
+INSERT INTO Catalog(sid, pid) VALUES (10002, 20005);
+SELECT *FROM CATALOG;
+
+
